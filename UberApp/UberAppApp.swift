ddd0f613922102e2364db7b174bc072898ed1b20
Aram <aramsaribekyan1@gmail.com>
2024-06-03 14:28:20 +0400

    Initial Commit

---
+++ UberApp/UberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberAppApp.swift
+//  UberApp
+//
+//  Created by Aram on 03.06.24.
+//
+
+import SwiftUI
+
+@main
+struct UberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
